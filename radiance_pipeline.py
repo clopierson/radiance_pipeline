ab0054a41bc07d14ec168f4402efa1b3a1de7d78
Liam Zimmermann <zimmerml@oregonstate.edu>
2023-05-21 15:34:25 -0700

    lint to 9.33/10, include merge in the pipeline object

--- radiance_pipeline.py
+++ radiance_pipeline.py
@@ -5,18 +5,19 @@ images.
 import os
 import platform
 from pathlib import Path
-import time
 from dataclasses import dataclass
+# import time
 
 
-from submodules.radiance_pipeline.radiance_data import RadianceData
-from submodules.radiance_pipeline.logs import recordLog
+from .radiance_data import RadianceData
+# from submodules.radiance_pipeline.logs import recordLog
 
 
 radiance_pipeline_percent: int
 radiance_pipeline_status_text: str
 radiance_pipeline_finished: bool
 radiance_pipeline_cancelled: bool
+radiance_pipeline_error: bool
 
 @dataclass
 class PipelineStage:
@@ -68,6 +69,25 @@ def vignetting_cmd(os_name, src, dest, session_data):
     return (f"pcomb -f {session_data.path_vignetting} -e 'diameter={session_data.diameter}' "
             f"{src} > {dest}")
 
+def clear_temp(output_path):
+    '''
+    Clear the temp directory of previous run's output files.
+    '''
+    cur_cmd = None
+    try:
+        # Loop through each intermediate output file path and remove it
+        for output_img in output_path[1:]:
+            cur_cmd = f"os.remove({output_img})"
+            os.remove(f"{output_img}")
+    except FileNotFoundError:
+        pass
+    except OSError as exc:
+        print(f"Error on command: {cur_cmd}")
+        print(f"ERROR: {exc}")
+        # recordLog(session_time, "ERROR", e)
+    finally:
+        pass
+
 def cancel_pipeline():
     '''
     Cancel pipeline in-between stages before it's fully finished. Use in thread.
@@ -83,13 +103,15 @@ def radiance_pipeline(session_data: RadianceData):
     global radiance_pipeline_status_text
     global radiance_pipeline_finished
     global radiance_pipeline_cancelled
+    global radiance_pipeline_error
     radiance_pipeline_finished = False
     radiance_pipeline_percent = 0
     radiance_pipeline_status_text = "Setting up..."
     radiance_pipeline_cancelled = False
+    radiance_pipeline_error = False
 
     # Grab time pipeline session started for log filenames, only want 1 per session
-    session_time = time.strftime("%Y%m%d-%H%M%S")
+    # session_time = time.strftime("%Y%m%d-%H%M%S")
 
     # Get OS Platform info
     os_name = os.name
@@ -103,8 +125,6 @@ def radiance_pipeline(session_data: RadianceData):
     print(f"OS Version: {os_version}")
     print("\n")
 
-
-
     # Ensure temp directory exists for storing intermediate output files from each pipeline step
     Path(session_data.path_temp).mkdir(mode=0o755, parents=True, exist_ok=True)
 
@@ -114,126 +134,106 @@ def radiance_pipeline(session_data: RadianceData):
     for i in range(1, 11):
         output_path.append(os.path.join(session_data.path_temp, f"output{i}.hdr"))
 
-    # --------------------------------------------------------------------------------------------
-    # Merging of exposures
-    radiance_pipeline_status_text = "Merging exposures (may take a while)"
-
     # Clear temp directory
-    try:
-        # Loop through each intermediate output file path and remove it
-        for output_img in output_path[1:]:
-            os.remove(f"{output_img}")
-            cur_cmd = f"os.remove({output_img})"
-    except FileNotFoundError:
-        pass
-    except OSError as e:
-        print(f"Error on command: {cur_cmd}")
-        recordLog(session_time, "ERROR", e)
-    finally:
-        pass
-
-    # Merge exposures
-    try:
-        radiance_pipeline_percent = 5
-        if session_data.path_rsp_fn is not None:
-            os.system(f"hdrgen {' '.join(session_data.paths_ldr)} -o {output_path[1]}"
-                      f" -r {session_data.path_rsp_fn} -a -e -f -g")
-        else:
-            os.system(f"hdrgen {' '.join(session_data.paths_ldr)} -o {output_path[1]}"
-                      f" -a -e -f -g")
-    except Exception as e:
-        recordLog(session_time, "ERROR", e)
-    finally:
-        print("Finished merging exposures.")
-        radiance_pipeline_percent = 10
-
-    # --------------------------------------------------------------------------------------------
+    clear_temp(output_path)
 
     pipeline = [
-      PipelineStage(cmd= f"ra_xyze -r -o {output_path[1]} > {output_path[2]}",
-                    altcmd=None,
-                    percent_difference=10,
-                    status_text="Nullifying exposures",
-                    finish_text="Finished nullifying exposures",
-                    skip=False),
+PipelineStage(cmd=f"hdrgen {' '.join(session_data.paths_ldr)} -o {output_path[1]}"
+              f" -r {session_data.path_rsp_fn} -a -e -f -g",
+              altcmd=f"hdrgen {' '.join(session_data.paths_ldr)} -o {output_path[1]}"
+              f" -a -e -f -g",
+              percent_difference = 5,
+              status_text="Merging exposures (may take a while)",
+              finish_text="Finished merging exposures",
+              skip=session_data.path_rsp_fn is None),
 
-      PipelineStage(cmd=f"pcompos -x {session_data.diameter} -y {session_data.diameter} "
-                        f"{output_path[2]} -{session_data.crop_x_left} "
-                        f"-{session_data.crop_y_down}, > {output_path[3]}",
-                    altcmd=None,
-                    percent_difference=10,
-                    status_text="Cropping",
-                    finish_text="Finished cropping",
-                    skip=False),
+PipelineStage(cmd= f"ra_xyze -r -o {output_path[1]} > {output_path[2]}",
+              altcmd=None,
+              percent_difference=10,
+              status_text="Nullifying exposures",
+              finish_text="Finished nullifying exposures",
+              skip=False),
 
-      PipelineStage(cmd=vignetting_cmd(os_name, output_path[3], output_path[4], session_data),
-                    altcmd=copy_cmd(os_name, output_path[3], output_path[4]),
-                    percent_difference=10,
-                    status_text="Correcting vignetting",
-                    finish_text="Finished correcting vignetting",
-                    skip=session_data.path_vignetting is None),
+PipelineStage(cmd=f"pcompos -x {session_data.diameter} -y {session_data.diameter} "
+                  f"{output_path[2]} -{session_data.crop_x_left} "
+                  f"-{session_data.crop_y_down}, > {output_path[3]}",
+              altcmd=None,
+              percent_difference=10,
+              status_text="Cropping",
+              finish_text="Finished cropping",
+              skip=False),
 
-      PipelineStage(cmd=f"pfilt -1 -x {session_data.target_x_resolution} -y "
-                    f"{session_data.target_y_resolution} {output_path[4]} > {output_path[5]}",
-                    altcmd=None,
-                    percent_difference=10,
-                    status_text="Resizing",
-                    finish_text="Finished resizing",
-                    skip=False),
+PipelineStage(cmd=vignetting_cmd(os_name, output_path[3], output_path[4], session_data),
+              altcmd=copy_cmd(os_name, output_path[3], output_path[4]),
+              percent_difference=10,
+              status_text="Correcting vignetting",
+              finish_text="Finished correcting vignetting",
+              skip=session_data.path_vignetting is None),
 
-      PipelineStage(cmd=f"pcomb -f {session_data.path_fisheye} {output_path[5]} > {output_path[6]}",
-                    altcmd=copy_cmd(os_name, output_path[5], output_path[6]),
-                    percent_difference=10,
-                    status_text="Adjusting fisheye",
-                    finish_text="Finished adjusting fisheye",
-                    skip=session_data.path_fisheye is None),
+PipelineStage(cmd=f"pfilt -1 -x {session_data.target_x_resolution} -y "
+                  f"{session_data.target_y_resolution} {output_path[4]} > {output_path[5]}",
+              altcmd=None,
+              percent_difference=10,
+              status_text="Resizing",
+              finish_text="Finished resizing",
+              skip=False),
 
-      PipelineStage(cmd=f"pcomb -f {session_data.path_ndfilter} {output_path[6]} > "
-                        f"{output_path[7]}",
-                    altcmd=copy_cmd(os_name, output_path[6], output_path[7]),
-                    percent_difference=10,
-                    status_text="Correcting neutral density filter",
-                    finish_text="Finished correcting neutral density filter",
-                    skip=session_data.path_ndfilter is None),
+PipelineStage(cmd=f"pcomb -f {session_data.path_fisheye} {output_path[5]} > {output_path[6]}",
+              altcmd=copy_cmd(os_name, output_path[5], output_path[6]),
+              percent_difference=10,
+              status_text="Adjusting fisheye",
+              finish_text="Finished adjusting fisheye",
+              skip=session_data.path_fisheye is None),
 
-      PipelineStage(cmd=f"pcomb -h -f {session_data.path_calfact} {output_path[7]} > "
-                        f"{output_path[8]}",
-                    altcmd=copy_cmd(os_name, output_path[7], output_path[8]),
-                    percent_difference=10,
-                    status_text="Performing photometric adjustment",
-                    finish_text="Finished photometric adjustment",
-                    skip=session_data.path_calfact is None),
+PipelineStage(cmd=f"pcomb -f {session_data.path_ndfilter} {output_path[6]} > "
+                  f"{output_path[7]}",
+              altcmd=copy_cmd(os_name, output_path[6], output_path[7]),
+              percent_difference=10,
+              status_text="Correcting neutral density filter",
+              finish_text="Finished correcting neutral density filter",
+              skip=session_data.path_ndfilter is None),
 
-    PipelineStage(cmd=f"(getinfo < {output_path[8]} | sed \"/VIEW/d\" && getinfo - < "
+PipelineStage(cmd=f"pcomb -h -f {session_data.path_calfact} {output_path[7]} > "
+                  f"{output_path[8]}",
+              altcmd=copy_cmd(os_name, output_path[7], output_path[8]),
+              percent_difference=10,
+              status_text="Performing photometric adjustment",
+              finish_text="Finished photometric adjustment",
+              skip=session_data.path_calfact is None),
+
+PipelineStage(cmd=f"(getinfo < {output_path[8]} | sed \"/VIEW/d\" && getinfo - < "
                   f"{output_path[8]}) > {output_path[9]}",
-                altcmd=None,
-                percent_difference=5,
-                status_text="Editing header",
-                finish_text="Finished editing image header",
-                skip=None),
+              altcmd=None,
+              percent_difference=5,
+              status_text="Editing header",
+              finish_text="Finished editing image header",
+              skip=None),
 
-    PipelineStage(cmd=f"getinfo -a \"VIEW = -vta "
-                f"-view_angle_vertical {session_data.view_angle_vertical} "
-                f"-view_angle_horizontal {session_data.view_angle_horizontal}\" "
-                f"< {output_path[9]} > {output_path[10]}",
-                altcmd=None,
-                percent_difference=5,
-                status_text="Adjusting for real viewing angle",
-                finish_text="Finished adjusting for real view angle",
-                skip=None),
+PipelineStage(cmd=f"getinfo -a \"VIEW = -vta "
+                  f"-view_angle_vertical {session_data.view_angle_vertical} "
+                  f"-view_angle_horizontal {session_data.view_angle_horizontal}\" "
+                  f"< {output_path[9]} > {output_path[10]}",
+              altcmd=None,
+              percent_difference=5,
+              status_text="Adjusting for real viewing angle",
+              finish_text="Finished adjusting for real view angle",
+              skip=None),
 
-    PipelineStage(cmd=f"evalglare -V {output_path[10]}",
-                altcmd=None,
-                percent_difference=10,
-                status_text="Performing validity check",
-                finish_text="Finished output image validity check",
-                skip=None)
+PipelineStage(cmd=f"evalglare -V {output_path[10]}",
+              altcmd=None,
+              percent_difference=10,
+              status_text="Performing validity check",
+              finish_text="Finished output image validity check",
+              skip=None)
     ]
 
     for stage in pipeline:
         # Check if pipeline got cancelled in between steps
         if radiance_pipeline_cancelled:
-            print("Cancelling active pipeline...\n")
+            print("Cancelling active pipeline...")
+            break
+        if radiance_pipeline_error:
+            print("Error, cancelling active pipeline...")
             break
 
         radiance_pipeline_status_text = stage.status_text
@@ -243,9 +243,10 @@ def radiance_pipeline(session_data: RadianceData):
                 os.system(stage.cmd)
             else:
                 os.system(stage.altcmd)
-        except Exception as e:
-            recordLog(session_time, "ERROR", e)
-            print(f"Radiance pipeline error: {e}")
+        except Exception as exc:
+            radiance_pipeline_status_text = f"Error on \"{stage.status_text}\": {exc}"
+            print(radiance_pipeline_status_text)
+            radiance_pipeline_cancelled = True
         finally:
             print(stage.finish_text)
             radiance_pipeline_percent += stage.percent_difference
