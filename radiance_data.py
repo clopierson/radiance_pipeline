1520df195e2f091fcbee87ee497b92a69ed260cf
Liam Zimmermann <zimmerml@oregonstate.edu>
2023-05-23 19:25:34 -0700

    radiance_data into a dataclass

--- radiance_data.py
+++ radiance_data.py
@@ -1,36 +1,20 @@
-class RadianceData:
-  def __init__(self, 
-          diameter,
-          crop_x_left,
-          crop_y_down,
-          view_angle_vertical,
-          view_angle_horizontal,
-          target_x_resolution,
-          target_y_resolution,
-          paths_ldr,
-          path_temp,
-          path_rsp_fn,
-          path_vignetting,
-          path_fisheye,
-          path_ndfilter,
-          path_calfact,
-          path_errors,
-          path_logs):
-            self.diameter         = diameter
-            self.crop_x_left            = crop_x_left
-            self.crop_y_down            = crop_y_down
-            self.view_angle_vertical               = view_angle_vertical
-            self.view_angle_horizontal               = view_angle_horizontal
-            self.target_x_resolution          = target_x_resolution
-            self.target_y_resolution          = target_y_resolution
-            self.paths_ldr        = paths_ldr
-            self.path_temp        = path_temp
-            self.path_rsp_fn      = path_rsp_fn
-            self.path_vignetting  = path_vignetting
-            self.path_fisheye     = path_fisheye
-            self.path_ndfilter    = path_ndfilter
-            self.path_calfact     = path_calfact
-            self.path_errors         = path_errors
-            self.path_logs         = path_logs
-            return
+from dataclasses import dataclass
 
+@dataclass
+class RadianceData:
+    diameter: int = None
+    crop_x_left: int = None
+    crop_y_down: int = None
+    view_angle_vertical: int = None
+    view_angle_horizontal: int = None
+    target_x_resolution: int = None
+    target_y_resolution: int = None
+    paths_ldr: [str] = None
+    path_temp: str = None
+    path_rsp_fn: str = None
+    path_vignetting: str = None
+    path_fisheye: str = None
+    path_ndfilter: str = None
+    path_calfact: str = None
+    path_errors: str = None
+    path_logs: str = None
